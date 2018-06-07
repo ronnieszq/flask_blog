3384080e1f729b0043e5363eecb2c5cb1265418f
ronnieszq <ronnieszq@hotmail.com>
2018-06-07 01:35:03 -0400

    initial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '7a58c03dd0aeb23a0e3b6b2d09aea832'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'ronniemiao@gmail.com'
+    MAIL_PASSWORD = '2610zjMAY1st'
